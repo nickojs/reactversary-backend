1b04c199e6b70200aba2e15340a5f865b557bbe0
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-09 20:57:56 -0300

    Created algorithm to generate better validation error responses

--- helpers/validate-routes.js
+++ helpers/validate-routes.js
@@ -3,14 +3,36 @@ const ErrorHandler = require('../models/http-error');
 
 module.exports = {
   default: (req, res, next) => {
-    const errors = validationResult(req);
-    const message = errors
-      .array()
-      .map((e) => e.msg)
-      .join(', ');
-    if (!errors.isEmpty()) return next(new ErrorHandler(message, 422));
+    const validation = validationResult(req);
+
+    // get unique fields with error and its relative message
+    const { errors } = validation;
+    const errorsLength = errors.length;
+    const errorsObj = { };
+
+    // loop through the errors to generate an error object
+    // with unique keys and all corresponding messages
+    for (let i = 0; i < errorsLength; i++) {
+      const currentMsg = errors[i].msg;
+      const currentParam = errors[i].param;
+
+      const checkExistingParam = Object
+        .keys(errorsObj)
+        .filter((key) => key === currentParam);
+
+      if (checkExistingParam.length > 0) {
+        const existingMsgs = errorsObj[currentParam];
+        errorsObj[currentParam] = [...existingMsgs, currentMsg];
+      } else {
+        errorsObj[currentParam] = [currentMsg];
+      }
+    }
+
+    if (errorsLength > 0) return res.status(422).json({ errors: errorsObj });
+
     next();
   },
+
   image: (req, res, next) => {
     if (!req.file) {
       return next(new ErrorHandler('No image provided', 422));
