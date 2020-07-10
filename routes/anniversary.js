2277e20e90b6cde43bc8eb6e65a30fa4dab7f31c
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-09 21:25:55 -0300

    Finished mocking anniversary main route

--- routes/anniversary.js
+++ routes/anniversary.js
@@ -4,12 +4,30 @@ const { signupSchema } = require('../validators/auth');
 const validateResult = require('../helpers/validate-routes');
 const isAuth = require('../middlewares/is-auth');
 
+const User = require('../models/User');
+
 const router = express.Router();
 
 router.post('/save-birthdate',
-  validateResult.default,
-  (req, res, next) => {
-    res.status(200).json({ message: 'ok' });
-  });
+  isAuth,
+  async (req, res, next) => {
+    const { userId } = req;
+    const {
+      name, date, gift, location
+    } = req.body;
+
+    const user = await User.getUserById(userId);
+
+    const birthday = {
+      name, date, gift, location
+    };
+
+    // user.addAnniversary(birthday);
+    // user.save();
+
+    res.status(200).json({ message: 'ok', user: req.userId });
+  },
+  // schema,
+  validateResult.default);
 
 module.exports = router;
