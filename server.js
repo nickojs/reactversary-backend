afa1a9b7fc5afb03081808d28b292d23f0e427ff
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-09 22:03:53 -0300

    Added associations between models on initDatabase method

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const errorHandler = require('./middlewares/error-handler');
 const authRoutes = require('./routes/auth');
 const anniversaryRoutes = require('./routes/anniversary');
 
+const User = require('./models/User');
+const Anniversary = require('./models/Anniversary');
+
 class Server {
   constructor(express) {
     this.express = express;
@@ -13,6 +16,8 @@ class Server {
   }
 
   initDatabase() {
+    User.hasMany(Anniversary);
+    Anniversary.belongsTo(User);
     return database.sync({ /* force: true */ });
   }
 
